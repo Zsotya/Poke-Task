518e909556fd4e4658ee6323ba5425142c5141a3
Zsotya <mocsok2000@gmail.com>
2024-06-17 13:07:31 +0200

    Initialized frontend and backend of the application

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="navbar">
+    <RouterLink to="/">Home</RouterLink>
+    <div class="auth-links" v-if="!loggedIn">
+      <RouterLink to="/login">Login</RouterLink>
+      <RouterLink to="/register">Register</RouterLink>
+    </div>
+    <div class="user-menu" v-else>
+      <span>Hello {{ user.name }}</span>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+const loggedIn = false;
+const user = {
+  name: "TestName",
+};
+</script>
+
+<style scoped>
+.navbar {
+  display: flex;
+}
+</style>

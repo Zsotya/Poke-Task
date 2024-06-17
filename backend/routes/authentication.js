6e45091c41f12f9052d7607e46d51637d7485ad9
Zsotya <mocsok2000@gmail.com>
2024-06-17 17:12:45 +0200

    Created login function via initialized JWT, created LoginView.vue

--- backend/routes/authentication.js
+++ backend/routes/authentication.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const db = require("../db");
 const router = express.Router();
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 // POST /api/register - Registration endpoint
 router.post("/api/register", async (req, res) => {
@@ -41,6 +45,49 @@ router.post("/api/register", async (req, res) => {
   } catch (error) {
     // Error handling during the request
     console.error("Error registering user:", error);
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+// POST /api/login - Login endpoint
+router.post("/api/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    // Check if username exists in the database
+    const checkUserQuery = "SELECT * FROM users WHERE username = ?";
+    db.query(checkUserQuery, [username], async (err, results) => {
+      if (err) {
+        console.error("Error checking user:", err);
+        return res.status(500).json({ error: "Server error" });
+      }
+
+      // User not found
+      if (results.length === 0) {
+        return res.status(401).json({ error: "This username doesn't exist" });
+      }
+
+      // Compare hashed password
+      const match = await bcrypt.compare(password, results[0].password);
+      if (!match) {
+        return res.status(401).json({ error: "Incorrect password" });
+      }
+
+      // Successful login
+      console.log("Successful login attempt");
+
+      // Generate token
+      const token = jwt.sign(
+        { username: results[0].username },
+        process.env.JWT_SECRET,
+        { expiresIn: "2h" }
+      );
+
+      // Respond with token
+      res.status(200).json({ token });
+    });
+  } catch (error) {
+    console.error("Error logging in:", error);
     res.status(500).json({ error: "Server error" });
   }
 });
